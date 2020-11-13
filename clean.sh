8cd4e2fb5e4930c86d3d7f19b04666202f2a15fe
Bachue Zhou <bahcue.shu@gmail.com>
2020-11-13 14:40:17 +0800

    ./clean.sh now will clean build folder

--- clean.sh
+++ clean.sh
@@ -2,4 +2,4 @@
 echo "Cleaning Build-cURL-nghttp2-quiche-android"
 rm -fr openssl/openssl nghttp2/nghttp2-1* quiche/{quiche,quiche-build} curl/curl-7* \
        {nghttp2,curl}/{arm,arm64,x86,x86_64} \
-       /tmp/openssl-* /tmp/nghttp2-* /tmp/curl-* all
+       /tmp/openssl-* /tmp/nghttp2-* /tmp/curl-* build
